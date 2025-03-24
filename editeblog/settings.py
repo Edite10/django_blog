60481ce743ff47d4b9ce40590e4bf310d9a09c49
Edite Budzilko <budzilkoedite@gmail.com>
2025-03-24 21:05:13 +0000

    Fix: ready to deploy to heroku

--- editeblog/settings.py
+++ editeblog/settings.py
@@ -26,9 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com', '8000-edite10-djangoblog-nc0m22uwwn4.ws-eu118.gitpod.io']
+CSRF_TRUSTED_ORIGINS = [ 'https://*.herokuapp.com']
 
 
 # Application definition
